5d1c5fb5c758651b541dc1fbd25caa4d1b576b40
Andreas Hässler <andreas.haessler@outlook.at>
2022-06-11 02:27:09 +0200

    Set line width to 120

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,7 @@ const Home: NextPage = () => {
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet" />
       </Head>
 
       <main className="p-6 flex flex-col items-center">
