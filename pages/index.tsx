9b185264399d586d4eeed1c752f656cc8e1f8215
Andreas Hässler <andreas.haessler@outlook.at>
2022-06-11 02:20:26 +0200

    Add nice font

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,13 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Andreas Haessler</title>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main className="p-6 flex flex-col items-center">
